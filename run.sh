0f6dd6bb64db8aea50bb68f4c246d0484de4091b
0xkekenputry <150660112+0xkekenputry@users.noreply.github.com>
2024-04-19 03:05:44 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-c436b9ee-79e6-4ac1-9c39-0166568ab992/)
 
 
 echo "$curl_output" >> Update.tsx
